51bfb63bdc7ab9d00076a9f00d560f96a7ef01c4
Thomas Brunström <tbn@du.se>
2020-01-27 09:51:17 +0100

    Added examples

--- files.py
+++ files.py
@@ -1,4 +1,4 @@
-
+import json
 def ReadFile(filename, path = './'):
     filelocation = path + filename
     try:
@@ -45,11 +45,53 @@ def WriteToFile(filname, path = './'):
         file_obj.write('Här skriver vi en rad')
         file_obj.write('Här skriver vi en till rad')
 
+def WriteToFilePi(filename, path = './', data = ''):
+    filelocation = path + filename
+    with open(filelocation, 'w') as file_obj:
+        file_obj.write(data)
 
-ReadFile('data.txt')
-ReadFile('data.txt', 'data_file/')
 
-list = ReadEachLine('data.txt', 'data_file/')
+def WriteToFilPiJson(data):
+    filename = './data_file/pi.json'
+    with open(filename, 'w') as file_obj:
+        json.dump(data, file_obj)  
+
+def ReadToFilePiFromJson():
+    filename = './data_file/pi.json'
+    with open(filename, 'r') as file_obj:
+        list = json.load(file_obj)
+
+    for i in range(0, 19):
+        print(list[i])         
+
+def ToDick():
+    filename = './data_file/pi.txt'
+    with open(filename, 'r') as file_obj:
+        content = file_obj.read()
+        l = content.split('.')
+        dick = { 'heltal' : l[0] + '.', 'decimaler' : l[1] }
+        with open('./data_file/ksksks.json', 'w') as ff_p:
+            json.dump(dick, ff_p)
+def SplitPi(): 
+    pi = ReadFile('pi.txt', 'data_file/')
+    i = 0
+    list = []
+    numbers = ''
+    for c in pi:
+        i += 1
+        numbers += str(c)
+        if i == 20:
+            list.append(numbers)
+            i = 0
+            numbers = ''
+    
+    WriteToFilePi(filename = 'pidivided.txt', data = '-'.join(list), path = './data_file/')
+    WriteToFilPiJson(list)
+
+# ReadFile('data.txt')
+# ReadFile('data.txt', 'data_file/')
+
+# list = ReadEachLine('data.txt', 'data_file/')
 # #Skriver ut dubbla newlines eftersom filen innehåller det och print skriver ut ett också.
 # for line in list: 
 #     print(line)
@@ -62,6 +104,8 @@ list = ReadEachLine('data.txt', 'data_file/')
 # for line in list2:
 #     print(line)
 
-# FindBirthdayInPi()
-
-WriteToFile('writetofile.txt', 'data_file/')
+#FindBirthdayInPi()
+#SplitPi()
+#ReadToFilePiFromJson()
+ToDick()
+# WriteToFile('writetofile.txt', 'data_file/')
